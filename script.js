47cf69f23c38f81b89ebc3e82d51a9b17139f77c
Yulo97 <giuliano_devito@hotmail.com>
2023-02-02 12:24:16 -0300

    Se agrego funcioncionalidad de filtrar por categorias de productos

--- script.js
+++ script.js
@@ -10,33 +10,18 @@ let carrito = [];
 
 document.addEventListener('DOMContentLoaded', () => {
     if (paginaIndex) {
-        fetch('data/productos.json')
-            .then(response => response.json())
-            .then(data => {
-                for (const product of data) {
-                    const elemento = document.createElement('div');
-                    elemento.classList.add('tarjetas')
-                    elemento.innerHTML = `<div class="card" style="width: 18rem;">
-                <img src="${product.imagen}" class="card-img-top">
-                <div class="card-body justify-content-center">
-                  <h5 class="card-title">${product.nombre}</h5>
-                  <p class="card-text">Categoria: ${product.categoria}</p>
-                  <p class="card-text">Precio: $<span class="producto-precio">${product.precio}</span></p>
-                  <a href="#" class="btn btn-secundario btn-ver-producto">Ver</a>
-                  <a href="#" class="btn btn-primario btn-agregar-carrito">Agregar al Carrito</a>
-                  <span class="product-id">${product.id}</span>
-                </div>
-              </div>`
-                    catalogoProductos.appendChild(elemento);
-                }
-            })
-            .catch(err => console.log(err))
-
-        if (localStorage.getItem("carrito")) {
-            carrito = JSON.parse(localStorage.getItem("carrito"))
-            actualizarCarrito()
-            actualizarTotalCarrito()
+        if(!localStorage.getItem('menuCategoria')){
+            traerProductos()
         }
+        else if(localStorage.getItem('menuCategoria')){
+            traerProductos(localStorage.getItem('menuCategoria'))
+            localStorage.removeItem('menuCategoria')
+        }
+        paginaIndex.addEventListener('click', e => {
+            if(e.target.classList.contains('categoria__producto')){
+                traerProductos(e.target.textContent.toUpperCase())
+            }
+        })
     }
     else if (paginaDetalle) {
 
@@ -54,14 +39,65 @@ document.addEventListener('DOMContentLoaded', () => {
 
         actualizarCarrito()
         actualizarTotalCarrito()
+
+        paginaDetalle.addEventListener('click', e => {
+            if(e.target.classList.contains('categoria__producto')){
+                window.location.pathname = "index.html";
+                localStorage.setItem('menuCategoria', e.target.textContent.toUpperCase())
+            }
+        })
     }
 })
 
-// const traerProductos = (categoria) => {
-// if (categoria == null){
+const traerProductos = (categoria) => {
+    catalogoProductos.innerHTML = "";
 
-// }
-// }
+    fetch('data/productos.json')
+        .then(response => response.json())
+        .then(data => {
+            if (categoria == null) {
+                for (const product of data) {
+                    const elemento = document.createElement('div');
+                    elemento.classList.add('tarjetas')
+                    elemento.innerHTML = `<div class="card" style="width: 18rem;">
+            <img src="${product.imagen}" class="card-img-top">
+            <div class="card-body justify-content-center">
+              <h5 class="card-title">${product.nombre}</h5>
+              <p class="card-text">Categoria: ${product.categoria}</p>
+              <p class="card-text">Precio: $<span class="producto-precio">${product.precio}</span></p>
+              <a href="#" class="btn btn-secundario btn-ver-producto">Ver</a>
+              <a href="#" class="btn btn-primario btn-agregar-carrito">Agregar al Carrito</a>
+              <span class="product-id">${product.id}</span>
+            </div>
+            </div>`
+                    catalogoProductos.appendChild(elemento);
+                }
+            }
+
+            else {
+                for (const product of data) {
+                    if (categoria.toUpperCase() == product.categoria.toUpperCase()) {
+                        const elemento = document.createElement('div');
+                        elemento.classList.add('tarjetas')
+                        elemento.innerHTML = `<div class="card" style="width: 18rem;">
+                <img src="${product.imagen}" class="card-img-top">
+                <div class="card-body justify-content-center">
+                  <h5 class="card-title">${product.nombre}</h5>
+                  <p class="card-text">Categoria: ${product.categoria}</p>
+                  <p class="card-text">Precio: $<span class="producto-precio">${product.precio}</span></p>
+                  <a href="#" class="btn btn-secundario btn-ver-producto">Ver</a>
+                  <a href="#" class="btn btn-primario btn-agregar-carrito">Agregar al Carrito</a>
+                  <span class="product-id">${product.id}</span>
+                </div>
+              </div>`
+                        catalogoProductos.appendChild(elemento);
+                    }
+                }
+            }
+        })
+        .catch(err => console.log(err))
+}
+
 
 // agregar producto al carrito
 if (paginaIndex) {
@@ -107,8 +143,8 @@ if (paginaIndex) {
         }
     })
 }
-else if(paginaDetalle){
-    
+else if (paginaDetalle) {
+
 }
 
 // eliminar producto del carrito
@@ -138,7 +174,7 @@ listaModalCarrito.addEventListener('click', e => {
 
 // actualizar HTML de carrito
 const actualizarCarrito = () => {
-    if(paginaDetalle){
+    if (paginaDetalle) {
         carrito = JSON.parse(localStorage.getItem('carrito'))
     }
 
